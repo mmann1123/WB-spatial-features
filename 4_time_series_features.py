de53041928d488deda65dc35945c5c53206a2be5
mmann1123 <mmann1123@gmail.com>
2024-04-14 22:03:18 -0400

    write red and nir to single band

--- 4_time_series_features.py
+++ 4_time_series_features.py
@@ -18,15 +18,21 @@ images
 def write_bands(image):
     with gw.open(image) as src:
         for i in range(len(src)):
+            # only keep bands 3,4  because want red and nir
+            if i not in [2, 3]:
+                continue
             # make folder for each band
-            os.makedirs(f"../single_band_mosaics/B{i}", exist_ok=True)
+
+            band_dict = {2: "B4", 3: "B8"}
+
+            os.makedirs(f"../single_band_mosaics/{band_dict[i]}", exist_ok=True)
 
             # write single band images
             gw.save(
                 src[i].astype(int16),
                 compress="LZW",
-                filename=f"../single_band_mosaics/B{i}/{os.path.basename(image).split('.')[0]}"
-                + f"_B{i}.tif",
+                filename=f"../single_band_mosaics/{band_dict[i]}/{band_dict[i]}_{os.path.basename(image).split('.')[0]}"
+                + f".tif",
                 num_workers=1,
                 overwrite=True,
             )
