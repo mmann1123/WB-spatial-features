2e4a3542e0f3978aefb06c5062753c1dfd352b56
mmann1123 <mmann1123@gmail.com>
2024-04-16 13:28:14 -0400

    add 1 band interp

--- 1_create_mosaics.py
+++ 1_create_mosaics.py
@@ -1,10 +1,99 @@
-# %%
+#%% Interpolate missing values in the time series
+
+from xr_fresh.interpolate_series import interpolate_nan
+from numpy import nan
+
+missing_data = nan
+
+bands = [
+    "B2",
+    "B3",
+    "B4",
+    "B8",
+    "B11",
+    "B12",
+] 
+
+os.chdir("/mnt/bigdrive/Dropbox/wb_malawi")
+os.makedirs("interpolated", exist_ok=True)
+
+north_tiles = sorted(glob("./tiles/*S2_SR_*_north*.tif"))
+
+south_tiles = sorted(glob("./tiles/*S2_SR_*_south*.tif"))
+south_tiles
+
+# Get unique grid codes
+pattern = r"(?<=-)\d+-\d+(?=\.tif)"
+unique_grids = list(
+    set(
+        [
+            re.search(pattern, file_path).group()
+            for file_path in south_tiles
+            if re.search(pattern, file_path)
+        ]
+    )
+)
+unique_grids
+
+# get unique year and quarter
+pattern = r"\d{4}_Q\d{2}"
+unique_quarters = sorted(list(
+    set(
+        [
+            re.search(pattern, file_path).group()
+            for file_path in south_tiles
+            if re.search(pattern, file_path)
+        ]
+    )
+))
+unique_quarters
+
+# add north tiles to the unique grids
+unique_grids += north_tiles
+
+for band_name in bands:
+    files = f"./{band_name}"
+    file_glob = f"{files}/*.tif"
+
+    f_list = sorted(glob(file_glob))
+    print(f_list)
+
+    for grid in unique_grids:
+        print("working on grid", grid)
+        a_grid = sorted([f for f in files if grid in f])
+        print('files:' , a_grid)
+        with gw.series(
+            a_grid,
+            transfer_lib="numpy",
+            window_size=[512, 512],
+        ) as src:
+            src.apply(
+                func=interpolate_nan(
+                    interp_type="linear",
+                    missing_value=missing_data,
+                    count=len(src.filenames),
+                ),
+                outfile=os.path.join(
+                os.getcwd(),
+                "interpolated",
+                f"{band_name}_S2_SR_linear_interp_{grid}.tif",
+            ),
+                num_workers=1,  # src.nchunks,
+                bands=1,
+                kwargs={"BIGTIFF": "YES"},
+            )
+
+# %% 
 import geowombat as gw
 from glob import glob
 import os
 import re
 
+
 os.chdir("/mnt/bigdrive/Dropbox/wb_malawi")
+os.makedirs("interpolated", exist_ok=True)
+
+
 south_tiles = sorted(glob("./tiles/S2_SR_*_south*.tif"))
 south_tiles
 
@@ -60,37 +149,5 @@ for quarter in unique_quarters:
 #%%
         
 
-# # %% interpolate missing doesn't work with multi band images
-# from xr_fresh.interpolate_series import interpolate_nan
-# from numpy import nan
-# !mkdir -p interpolated
-
-# missing_data = nan
-
-# # Print the unique codes
-# for grid in unique_grids:
-#     print("working on grid", grid)
-#     a_grid = sorted([f for f in south_tiles if grid in f])
-#     print('files:' , a_grid)
-#     with gw.series(
-#         a_grid,
-#         transfer_lib="numpy",
-#         window_size=[512, 512],
-#     ) as src:
-#         src.apply(
-#             func=interpolate_nan(
-#                 interp_type="linear",
-#                 missing_value=missing_data,
-#                 count=len(src.filenames),
-#             ),
-#             outfile=os.path.join(
-#             os.getcwd(),
-#             "interpolated",
-#             f"S2_SR_linear_interp_{grid}.tif",
-#         ),
-#             num_workers=1,  # src.nchunks,
-#             bands=1,
-#             kwargs={"BIGTIFF": "YES"},
-#         )
 
 # %%
